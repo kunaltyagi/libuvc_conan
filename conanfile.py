c9e41f57346c41413bb2b876f168f85bc0d5811d
Kunal Tyagi <tyagi.kunal@live.com>
2020-05-09 07:33:34 +0900

    package succeeded

--- conanfile.py
+++ conanfile.py
@@ -10,26 +10,35 @@ class LibuvcConan(ConanFile):
     description = "a cross-platform library for USB video devices"
     topics = ("c", "camera", "usb")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
+    options = {"shared": [True, False],
+               "fPIC": [True, False], }
+    default_options = {"shared": True,
+                       "fPIC": True, }
     generators = "cmake"
+    _cmake = None
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(source_folder="libuvc")
+        return self._cmake
 
     def source(self):
         self.run("git clone https://github.com/libuvc/libuvc.git")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="libuvc")
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="libuvc/src")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = self._configure_cmake()
+        cmake.install()
+        cmake.patch_config_paths()
+        self.copy("LICENSE.txt", src="libuvc", dst="licenses")
 
     def package_info(self):
-        self.cpp_info.libs = ["libuvc"]
-
+        self.cpp_info.names["cmake_find_package"] = "libuvc"
+        self.cpp_info.names["cmake_find_package_multi"] = "libuvc"
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ["include/libuvc/"]
